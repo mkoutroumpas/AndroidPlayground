212e309eeaf45408b19dd0171a59d68b8a368732
Marios Koutroumpas <mkoutroumpas@gmail.com>
2020-03-06 22:58:12 +0100

    Initial commit.

---
+++ AndroidPlayground/CustomShapeView.cs
@@ -0,0 +1,76 @@
+﻿using Android.Content;
+using Android.Graphics.Drawables;
+using Android.Runtime;
+using Android.Util;
+using Android.Views;
+using Android.Widget;
+using FFImageLoading.Cross;
+using System;
+
+namespace PlaygroundTest
+{
+    [Register("playgroundtest." + nameof(CustomShapeView))]
+    public class CustomShapeView : RelativeLayout
+    {
+        private MvxCachedImageView _imageView;
+
+        protected CustomShapeView(IntPtr javaReference, JniHandleOwnership transfer)
+            : base(javaReference, transfer) { }
+
+        public CustomShapeView(Context context) : base(context)
+        {
+            Init(context);
+        }
+
+        public CustomShapeView(Context context, IAttributeSet attrs) : base(context, attrs)
+        {
+            Init(context, attrs);
+        }
+
+        public CustomShapeView(Context context, IAttributeSet attrs, int defStyleAttr) : base(context, attrs,
+            defStyleAttr)
+        {
+            Init(context, attrs);
+        }
+
+        public CustomShapeView(Context context, IAttributeSet attrs, int defStyleAttr, int defStyleRes) : base(context,
+            attrs, defStyleAttr, defStyleRes)
+        {
+            Init(context, attrs);
+        }
+
+        private void Init(Context context, IAttributeSet attrs = null)
+        {
+            ClipToOutline = true;
+            //Elevation = TypedValue.ApplyDimension(ComplexUnitType.Dip, 18, context.Resources.DisplayMetrics);
+
+            var layoutParams = new LayoutParams(ViewGroup.LayoutParams.WrapContent, ViewGroup.LayoutParams.WrapContent);
+            layoutParams.AddRule(LayoutRules.CenterInParent);
+
+            _imageView = new MvxCachedImageView(context)
+            {
+                LayoutParameters = layoutParams
+            };
+
+            AddView(_imageView);
+        }
+
+        public void SetImageFromFilePath(string filepath)
+        {
+            if (_imageView != null)
+            {
+                _imageView.ImagePath = filepath;
+            }
+        }
+
+        public void SetImageFromResource(int resourceId)
+        {
+            _imageView?.SetImageResource(resourceId);
+        }
+
+        public override void SetBackgroundColor(Android.Graphics.Color color)
+        {
+            (Background as GradientDrawable)?.SetColor(color);
+        }
+    }
+}
---
+++ AndroidPlayground/MainActivity.cs
@@ -0,0 +1,45 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Views;
+
+namespace PlaygroundTest
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme.NoActionBar", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.activity_main);
+
+            Android.Support.V7.Widget.Toolbar toolbar = FindViewById<Android.Support.V7.Widget.Toolbar>(Resource.Id.toolbar);
+            SetSupportActionBar(toolbar);
+
+            #region CustomShapeView example
+            var csvCustomShapeView = FindViewById<CustomShapeView>(Resource.Id.csvCustomShapeView);
+            csvCustomShapeView.SetImageFromResource(Resource.Drawable.home);
+            csvCustomShapeView.SetBackgroundColor(Resources.GetColor(Resource.Color.white));
+            #endregion
+        }
+
+        public override bool OnCreateOptionsMenu(IMenu menu)
+        {
+            MenuInflater.Inflate(Resource.Menu.menu_main, menu);
+            return true;
+        }
+
+        public override bool OnOptionsItemSelected(IMenuItem item)
+        {
+            int id = item.ItemId;
+            if (id == Resource.Id.action_settings)
+            {
+                return true;
+            }
+
+            return base.OnOptionsItemSelected(item);
+        }
+	}
+}
+
